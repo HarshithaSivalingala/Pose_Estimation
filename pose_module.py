9cad9b2a8b16445589e6ffcf93bc57995d4d162d
pratyusha05 <pratyushap05@gmail.com>
2021-04-19 18:27:07 +0530

    added main

--- pose_module.py
+++ pose_module.py
@@ -34,3 +34,25 @@ class poseDetector():
                 if draw:
                     cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
         return lmList
+
+def main():
+    cap = cv2.VideoCapture('exercise1.mp4')
+    pTime = 0
+    detector = poseDetector()
+    while True:
+        success, img = cap.read()
+        img = detector.findPose(img)
+        lmList = detector.findPosition(img)
+        #print(lmList)
+        cTime  = time.time()
+
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+
+        cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+        cv2.imshow("Image", img)
+
+        cv2.waitKey(1)
+
+if __name__ == "__main__":
+    main()
