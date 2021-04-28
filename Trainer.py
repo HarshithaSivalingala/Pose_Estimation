2405a24f1567f34071279b97fe1e9602cd6bcbf0
sritulasi <19wh1a0488@bvrithyderabad.edu.in>
2021-04-28 21:40:39 +0530

    Made changes for squat code

--- Trainer.py
+++ Trainer.py
@@ -9,20 +9,22 @@ cap = cv2.VideoCapture("Dataset/curls.mp4")
 detector = pm.poseDetector()
 count = 0
 dir = 0
+pTime = 0
 while True:
     success, img = cap.read()
-    img = cv2.resize(img, (1000, 700))
+    img = cv2.resize(img, (1300, 720))
 
-    img = detector.findPose(img, True)
+    img = detector.findPose(img, False)
     lmList = detector.findPosition(img, True)
 
     if len(lmList) != 0:
         # Right Arm
-        #angle = detector.findAngle(img, 12, 14, 16)
+        angle = detector.findAngle(img, 12, 14, 16)
         # Left Arm
         angle = detector.findAngle(img, 11, 13, 15)
         per = np.interp(angle, (210, 310), (0, 100))
         bar = np.interp(angle, (220, 310), (650, 100))  # (min, max)
+        print(angle, per)
 
         # Check for the dumbbell curls
         color = (255, 0, 255)
@@ -45,6 +47,10 @@ while True:
         #Curls Count
         cv2.rectangle(img, (0, 570), (175, 720), (0, 255, 0), cv2.FILLED)
         cv2.putText(img, str(int(count)), (30, 700), cv2.FONT_HERSHEY_PLAIN, 10, (255, 0, 0), 20)
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+    cv2.putText(img, str(int(fps)), (50, 100), cv2.FONT_HERSHEY_PLAIN, 5, (255, 0, 0), 5)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
